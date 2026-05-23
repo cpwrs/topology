5ebbcaa983e3de3f7b539bc341a0d734ea71b218
Carson Powers <me@carsonp.net>
2026-05-22 20:31:45 -0500

    hosts/toaster: facter report

--- modules/hosts/toaster/hardware.nix
+++ modules/hosts/toaster/hardware.nix
@@ -1,5 +1,6 @@
 {...}: {
   flake.modules.nixos.toaster = {lib, ...}: {
+    hardware.facter.reportPath = ./facter.json;
     services.xserver.videoDrivers = lib.singleton "nvidia";
     hardware.nvidia = {
       open = true;
