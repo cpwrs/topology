ba670e776938561181b7bd0ba53b53915c3d789f
Carson Powers <me@carsonp.net>
2026-05-26 14:59:06 -0500

    core: packages on darwin

--- modules/core/packages.nix
+++ modules/core/packages.nix
@@ -1,19 +1,29 @@
-{
-  flake.modules.nixos.core = {pkgs, ...}: {
-    environment.systemPackages = [
-      pkgs.git
-      pkgs.wget
-      pkgs.zip
-      pkgs.unzip
-      pkgs.btop
-      pkgs.fd
-      pkgs.jq
-      pkgs.ripgrep
-      pkgs.binutils
-      pkgs.pciutils
-      pkgs.usbutils
-      pkgs.vim
-      pkgs.psmisc
-    ];
+let
+  packages = {
+    pkgs,
+    lib,
+    ...
+  }: {
+    environment.systemPackages =
+      [
+        pkgs.git
+        pkgs.wget
+        pkgs.zip
+        pkgs.unzip
+        pkgs.btop
+        pkgs.fd
+        pkgs.jq
+        pkgs.ripgrep
+        pkgs.vim
+      ]
+      ++ lib.optionals pkgs.stdenv.isLinux [
+        pkgs.binutils
+        pkgs.pciutils
+        pkgs.usbutils
+        pkgs.psmisc
+      ];
   };
+in {
+  flake.modules.nixos.core = packages;
+  flake.modules.darwin.core = packages;
 }
